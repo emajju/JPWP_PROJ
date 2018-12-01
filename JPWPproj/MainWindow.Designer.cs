f69cd9843df6eff8b88b12b81db221af95161228
Janek <Janek@DESKTOP-I51DARP>
2018-12-01 09:02:36 +0100

    Prepare base methods to use the screen and keyboard

---
+++ JPWPproj/MainWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace JPWPproj
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrPaintScreen = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrPaintScreen
+            // 
+            this.tmrPaintScreen.Enabled = true;
+            this.tmrPaintScreen.Interval = 20;
+            this.tmrPaintScreen.Tick += new System.EventHandler(this.tmrPaintScreen_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 985);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainWindow";
+            this.Text = "Tin Shooter";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainWindow_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrPaintScreen;
+    }
+}
+
---
+++ JPWPproj/MainWindow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPWPproj
+{
+    public partial class MainWindow : Form
+    {
+        private int _y;
+        private int _x;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _y = 50;
+            _x = 50;
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        { //First window load after start app
+            LoadFirstButtons(sender);
+        }
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        { //Drawing window
+            //Console.WriteLine("Paint");
+            e.Graphics.FillRectangle(Brushes.BurlyWood, _x, _y, 100, 100);
+        }
+
+        private void tmrPaintScreen_Tick(object sender, EventArgs e)
+        {
+           //Do all math here
+            Invalidate(); //Redraw window every 20ms 50fps
+        }
+
+        private void LoadFirstButtons(object sender)
+        {
+            var startButton = new Button();
+            startButton.Text = "Start";
+            Point loc = new Point(500, 200);
+            startButton.Location = loc;
+            startButton.Visible = true;
+                
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(e.KeyCode);
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+       
+    }
+}
